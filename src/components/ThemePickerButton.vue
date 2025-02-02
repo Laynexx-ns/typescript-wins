c3d3260db4f3d69fc2d79b5a563f128bfd9a895c
laynexx <argo150808@gmail.com>
2025-02-02 22:07:14 +0300

    fixed all bugs

--- src/components/ThemePickerButton.vue
+++ src/components/ThemePickerButton.vue
@@ -29,7 +29,7 @@ const handleClick = () =>{
 
 <template>
   <button @click="handleClick" id="btn" class="absolute flex items-center justify-center lg:ml-8 sm:ml-4 sm:mt-4 lg:mt-8 size-12 rounded-2xl border border-white/50">
-    <MoonIcon v-if="dark.value"/>
+    <MoonIcon v-if="dark"/>
     <Sun v-else />
   </button>
 </template>
